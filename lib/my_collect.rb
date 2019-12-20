dc0941c127718dc73acc19b9d46d0ce338ad2948
pledgedavis <pledgedavis14@gmail.com>
2019-12-20 18:34:15 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect(array)
+#   i = 0 
+#   collection = []
+#   while i < array.length
+#   yield.(array[i])
+#   i += 1
+# end
+# collection
 
 
+array = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(array) do |lang|
+  lang.upcase
+
+end
+end
